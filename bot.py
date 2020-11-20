6d8453e6f225c4db3db293c9fe682b5735039b1a
shane12345601 <shane12345601@gmail.com>
2020-11-19 20:56:18 -0400

    pH test

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands, tasks
-import pornhub_api
-from pornhub_api import PornhubApi
+#import pornhub_api
+#from pornhub_api import PornhubApi
 import imaplib, email, quopri
+from pornhub.pornhub_api import PornhubApi
 
 api = PornhubApi()
 
